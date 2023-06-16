28609a0adb50032f7d25549d43e16cb49fc8772b
Bafoe <kbafoe66@gmail.com>
2023-06-16 10:20:33 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*Writing a C program that prints exactly "Programming is like building a multilingual puzzle.
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
