a3d1bac4163bcc2759f94c6670dbd0cb67c3b4d8
Bafoe <kbafoe66@gmail.com>
2023-06-19 19:23:16 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,16 @@
 */
 int main(void)
 {
-int d;
-for (d = '0'; d < '9'; d++)
+int d, c;
+for (d = '0'; d <= '9'; d++)
 {
-putchar(d);
-if (d != '9')
-{
-putchar(',');
-putchar(' ');
-}
+	for (c = '0'; c <= '9'; c++)
+	{
+	putchar(d);
+	putchar(c);
+	putchar(',');
+	putchar(' ');
+	}
 }
 putchar('\n');
 return (0);
