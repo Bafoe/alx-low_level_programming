0d8d8922732e90031a81060a78573c3e2c220531
Bafoe <kbafoe66@gmail.com>
2023-06-27 15:01:27 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,15 @@
  */
 void puts2(char *str)
 {
-	int i,j;
+	int i, j;
 	j = 0;
 	while (str[j] != '\0')
 	{
-		j++
+		j++;
 	}
 	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
