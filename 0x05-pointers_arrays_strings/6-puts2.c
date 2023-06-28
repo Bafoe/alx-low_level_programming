168692f194c5728be7c391e8524bb5f7b5260181
Bafoe <kbafoe66@gmail.com>
2023-06-27 18:42:17 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,23 @@
 #include "main.h"
 /**
  *puts2 - Write a function that prints every other character of a string
- *@s : the string to print
+ *@str : the string to print
  *Return : void
  */
 void puts2(char *str)
 {
 	int i, j;
+
 	j = 0;
+
 	while (str[j] != '\0')
 	{
+
 		j++;
 	}
 	for (i = 0; i < j; i += 2)
 	{
+
 		_putchar(str[i]);
 	}
 	_putchar('\n');
