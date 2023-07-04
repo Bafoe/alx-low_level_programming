bc7cd99227758d33fedde80251b1525534efc57b
Bafoe <kbafoe66@gmail.com>
2023-07-03 19:14:06 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -22,26 +22,3 @@ char *_memset(char *s, char b, unsigned int n)
 
 	return s;
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char memory[10];  /* Example memory area */
-
-	/* Fill memory with the constant byte 'A' */
-	char *filled_memory = _memset(memory, 'A', 10);
-
-	/* Print the contents of memory */
-	for (int i = 0; i < 10; i++)
-	{
-		putchar(filled_memory[i]);
-		putchar(' ');
-	}
-
-	return 0;
-}
-
