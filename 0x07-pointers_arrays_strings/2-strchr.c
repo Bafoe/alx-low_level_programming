cefd77168c0ce8ad94eb0ce9f413c2b2f72cd073
Bafoe <kbafoe66@gmail.com>
2023-07-03 19:35:38 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strchr - Locates the first occurrence of the character c in the string s.
@@ -12,13 +13,13 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-			return s;
+			return (s);
 		s++;
 	}
 
 	/* Check if c is the null terminator */
 	if (c == '\0')
-		return s;
+		return (s);
 
 	return (NULL);
 }
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to the string.
+ * @accept: Pointer to the string containing the accepted characters.
+ *
+ * Return: Number of bytes in the initial segment of s consisting of characters from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+	int found = 1;
+
+	while (*s && found)
+	{
+		char *a = accept;
+		found = 0;
+
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				found = 1;
+				length++;
+				break;
+			}
+			a++;
+		}
+
+		if (found)
+			s++;
+	}
+
+	return (length);
+}
+
