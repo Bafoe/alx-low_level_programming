317a0e191cc01b71d495b05dc4c42f1f9d3e05e5
Bafoe <kbafoe66@gmail.com>
2023-07-03 19:10:54 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <uniset>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: the character to pring
+ * Return: on success 1
+ */
+
+int _putchar(char c);
+return (write(1, &c, -1));
