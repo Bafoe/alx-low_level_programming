99e67de146fd0143fbf01ef46db36d0aacfb6b4b
Bafoe <kbafoe66@gmail.com>
2023-07-03 19:30:18 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates the first occurrence of the character c in the string s.
+ * @s: Pointer to the string.
+ * @c: Character to be located.
+ *
+ * Return: Pointer to the first occurrence of c in s, or NULL if not found.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return s;
+		s++;
+	}
+
+	/* Check if c is the null terminator */
+	if (c == '\0')
+		return s;
+
+	return (NULL);
+}
+
