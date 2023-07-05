f95643d1073d8e12a9b75951ded70734d4ad033c
Bafoe <kbafoe66@gmail.com>
2023-07-05 11:47:04 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+/**
+ * print_alphabet_x10 - this prints alpabet ten times
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+int c;
+int i;
+for (c = 'a'; c <= 'z'; c++)
+{
+for (i = 0; i <= 10; i++)
+{
+_putchar('c');
+}
+_putchar('\n');
+}
+
+return (0);
+}
