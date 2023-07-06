19cac62c8668de0e95d57277ac472ee4c5da043a
Bafoe <kbafoe66@gmail.com>
2023-07-05 20:32:10 -0700

    0x08-recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints strings in reverse
+ * @*s: string
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if *s == '\0';
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(++s);
+		_putchar(s);
+	}
+	return (0);
+}
