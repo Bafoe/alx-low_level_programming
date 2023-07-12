3694041d87faa69b9d88afdc4f70322c7501a4cd
Bafoe <kbafoe66@gmail.com>
2023-07-11 18:25:50 -0700

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+char *create_array(unsigned int size, char c);
+/*
+ * creat_array - this function creats an array of char 
+ * and initialize it to specific bar
+ * @size : size of charater
+ * @c : string
+ * Return : return a char
+ */
+
+char *ptArray;
+
+ptArray = malloc(unsigned int size * sizeof(char));
+
+unsigned int count;
+count = 0;
+while(count < size)
+{
+	ptArray[count] = c;
+	count++;
+}
+return (ptArray);
