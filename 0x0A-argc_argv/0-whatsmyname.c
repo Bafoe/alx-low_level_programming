fdd153b21378255bfc361d9b69815c44f14ccdf1
Bafoe <kbafoe66@gmail.com>
2023-07-12 19:30:06 -0700

    command argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector containing strings of the args
+ *
+ * Return: 0 always succes
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[1]);
+return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: number of args
+ * @argv: the strings of the args
+ *
+ * Return: 0 for success 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2, p;
+
+	n1 = 0;
+	n2 = 0;
+	p = 0;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		p = n1 * n2;
+		printf("%d\n", p);
+	}
+	return (0);
+}
